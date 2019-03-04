59730ed1d2de33463c80e0ff0c8d77bed14fab35
bailongctui <bailongctui@gmail.com>
2019-03-04 13:56:51 +0800

    拆分Reducer 再  combineReducers
    拆分Reducer 为多个state，再通过combineReducers 整合为一个state

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,18 @@
-import {createStore} from 'redux';
-import { Reducer, initialState } from './reducer'
+import {createStore, combineReducers} from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer, // reducer
-        initialState, // our initialState
+        // 把一个由多个不同 reducer 函数作为 value 的 object，合并成一个最终的 reducer 函数，然后就可以对这个 reducer 调用 createStore。
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
     );
 
     return store;
