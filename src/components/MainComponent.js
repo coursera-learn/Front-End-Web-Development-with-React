f703f8fa76d3cc3071a68c1677dd859c09e91c06
sidney <bailongctui@users.noreply.github.com>
2022-03-16 10:54:05 +0800

    React Animations

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -12,6 +12,10 @@ import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
 import { actions } from "react-redux-form";
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
+
+
 
 const mapStateToProps = state => {
   return {
@@ -83,14 +87,18 @@ class Main extends Component {
       <div>
         <Header />
         <div>
-          <Switch>
-              <Route path='/home' component={HomePage} />
-              <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />} />
-              <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
-              <Route path='/menu/:dishId' component={DishWithId} />
-              <Route exact path='/contactus' component={ () => <Contact resetFeedBackForm={this.props.resetFeedBackForm} />} />} />
-              <Redirect to="/home" />
-          </Switch>
+          <TransitionGroup>
+              <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+                <Switch location={this.props.location}>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />} />
+                    <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
+                    <Route path='/menu/:dishId' component={DishWithId} />
+                    <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
+                    <Redirect to="/home" />
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
         </div>
         <Footer />
       </div>
